c42d95787f8221f9364ef378a776f7d106fc0272
Bitter Starglow <freym827@gmail.com>
2018-09-15 13:53:25 -0400

    little more

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+var restop = ["pizza hut", "dairy queen", "olive garden", "red lobster", "eatin park"];
+var drugop = ["CVS", "walgreens", "riteaid", "drug mart", "eatin park"]
+
+$("option").on("click", function() {
+    console.log("yay")
+});
