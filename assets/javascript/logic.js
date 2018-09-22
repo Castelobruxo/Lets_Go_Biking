5e679ee7c50acd85d84a19c33d785d604bb7872e
GadgetCopter <rjdobrz@gmail.com>
2018-09-22 11:59:05 -0400

    weather logic

---
+++ assets/javascript/logic.js
@@ -0,0 +1,71 @@
+var timeParse
+
+
+$(document).ready(function() {
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    
+    // set the map to the device's current location 
+    function showPosition(position) {
+        // console.log(position.coords.latitude) 
+        // console.log(position.coords.longitude)
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+       
+        var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&units=imperial&APPID=cb5acdd0786623637d642f126e5ae380"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+    
+
+            // console.log(time.format("HH:mm:ss"))
+    
+            // Logic for pushing cloud data and images to weather section
+            console.log(moment.unix(response.list[0].dt))
+                for (var i = 0; i < 4; i++) {
+                    
+                    timeParse();
+                   
+
+                    if (response.list[i].weather[0].id > 800) {
+                        $("#weatherimg-" + i).attr("src", "http://openweathermap.org/img/w/03d.png")
+                        
+                        } else if (response.list[i].weather[0].id === 800) {
+                            $('#weatherimg-' + i).attr("src", "http://openweathermap.org/img/w/01d.png")
+                        
+                        } else if (response.list[i].weather[0].id >= 200 && response.list[i].weather[0].id < 300) {
+                            $("#weatherimg-" + i).attr("src", "http://openweathermap.org/img/w01d.png")
+                            
+                        
+                        } else if (response.list[i].weather[0].id >= 300 && response.list[i].weather[0].id < 400) {
+                            $("weatherimg-" + i).attr("src", "http://openweathermap.org/img/w01d.png")
+                            
+                        
+                        } else if (response.list[i].weather[0].id >= 500 && response.list[i].weather[0].id < 600) {
+                            $("#weatherimg-" + i).attr("src", "http://openweathermap.org/img/w/09d.png")
+                        
+                        } else if (response.list[i].weather[0].id >= 600 && response.list[i].weather[0].id < 700) {
+                            $("weatherimg-" + i).attr("src", "http://openweathermap.org/img/w01d.png")
+                        }
+                }
+                
+                timeParse = function(){
+                    var time = moment.unix(response.list[i].dt)
+                    $(".t" + i).text(time.format("HH:mm"))
+                } 
+            });
+
+    }
+    
+    
+    getLocation()
+
+});
